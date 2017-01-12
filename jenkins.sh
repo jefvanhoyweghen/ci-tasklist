0e0877f9db815893843dbaac2ca60b042aaf8098
Jef Van Hoyweghen <jef@socialseeder.com>
2017-01-12 04:52:38 +0100

    jenkins bash script

--- jenkins.sh
+++ jenkins.sh
@@ -6,7 +6,6 @@ cp phpunit.xml.jenkins phpunit.xml
 composer self-update
 composer install --no-interaction
 php artisan key:generate
-mysql -u root -h localhost:33060
 cat .env
 php artisan migrate --seed
 vendor/bin/phpunit
