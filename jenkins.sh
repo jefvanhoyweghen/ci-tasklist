7a9403bf031ae2ada136dedc88690f0d4054be21
Jef Van Hoyweghen <jef@socialseeder.com>
2017-01-12 04:38:10 +0100

    jenkins bash script

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,10 @@
 FOLDER=$1
 cd $FOLDER
 rm .env
-cp /.env.jenkins /.env
+cp .env.jenkins .env
 cp phpunit.xml.jenkins phpunit.xml
 composer self-update
 composer install --no-interaction
 php artisan key:generate
 php artisan migrate --seed
-cat .env
 vendor/bin/phpunit
