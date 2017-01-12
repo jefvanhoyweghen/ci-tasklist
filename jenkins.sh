4f3b4e805b824b5be9d095f8f2a48c736b60a3fd
Jef Van Hoyweghen <jef@socialseeder.com>
2017-01-12 13:37:03 +0100

    remove ip from jenkins script

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,7 @@
 FOLDER=$1
-IP=$2
 cd $FOLDER
 rm .env
 cp .env.jenkins .env
-#echo "DB_TEST_HOST=$IP" >> .env
 cp phpunit.xml.jenkins phpunit.xml
 composer self-update
 composer install --no-interaction
