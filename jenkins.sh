4f382a8c9a9a1d5144a997bbfddc203f9fd2c6c3
Jef Van Hoyweghen <jef@socialseeder.com>
2017-01-12 04:57:04 +0100

    jenkins bash script

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,9 @@
 FOLDER=$1
+IP=$2
 cd $FOLDER
 rm .env
 cp .env.jenkins .env
+echo "DB_TEST_HOST=$IP" >> .env
 cp phpunit.xml.jenkins phpunit.xml
 composer self-update
 composer install --no-interaction
